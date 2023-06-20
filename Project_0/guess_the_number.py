a7a48d3e82fb6251f92e1125816c932c656e89b0
Simonenko S <stassi_gsm@mail.ru>
2023-06-20 19:21:07 +0300

    to save in git

--- Project_0/guess_the_number.py
+++ Project_0/guess_the_number.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def game_core_v2(number):
+def game_core_v3(number):
     """ Устанавливаем любое random число, а потом методом деления отрезка пополам ищет загаданное число.
         Функция принимает загаданное число и возвращает число попыток """
     count = 1
@@ -31,4 +31,4 @@ def score_game(game_core):
     return score
 
 
-score_game(game_core_v2)
+score_game(game_core_v3)
